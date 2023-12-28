daf81e27d9ac5a191856dc3ef73e6260ae908a88
Omochice <mochice.mls.ntl@gmail.com>
2023-12-28 18:41:57 +0900

    feat: add mod.ts

---
+++ mod.ts
@@ -0,0 +1,70 @@
+import type {
+  Data,
+  Engine,
+  Helper,
+  Page,
+  Site,
+} from "https://deno.land/x/lume@v1.19.3/core.ts";
+import { merge } from "https://deno.land/x/lume@v1.19.3/core/utils.ts";
+import loader from "https://deno.land/x/lume@v1.19.3/core/loaders/text.ts";
+import zenn from "npm:zenn-markdown-html@0.1.149";
+
+const markdownToHtml = zenn.default;
+
+export interface Options {
+  extensions: string[];
+  keepDefaultPlugins: boolean;
+}
+
+export const defaults: Options = {
+  extensions: [".md"],
+  keepDefaultPlugins: false,
+};
+
+export class MarkdownEngine implements Engine {
+  constructor() {}
+
+  deleteCache() {}
+
+  render(
+    content: string,
+    _data?: Data,
+    _filename?: string,
+  ): Promise<string> {
+    return Promise.resolve(markdownToHtml(content));
+  }
+
+  renderSync(
+    content: string,
+    _data?: Data,
+    _filename?: string,
+  ): string {
+    return markdownToHtml(content);
+  }
+
+  addHelper() {}
+}
+
+export default function (userOptions?: Partial<Options>) {
+  const options = merge(defaults, userOptions);
+
+  return function (site: Site) {
+    const engine = new MarkdownEngine();
+
+    site.loadPages(options.extensions, loader, engine);
+
+    function filter(string: string, _inline = false): string {
+      return engine.renderSync(string?.toString() || "").trim();
+    }
+
+    site.filter("md", filter as Helper);
+  };
+}
+
+export interface ZennPageData extends Page {
+  title: string;
+  emoji: string;
+  type: "tech" | "idea";
+  topics: string[];
+  published: boolean;
+}
