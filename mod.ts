a91663bec5f75b69b061db940f95d835cf13fb53
Omochice <44566328+Omochice@users.noreply.github.com>
2024-01-15 23:21:58 +0900

    feat!: migrate lume v2 (#9)
    * chore: add Exception compat layer
    
    * feat!: migrate v2
    
    Release-As: 0.2.0

--- mod.ts
+++ mod.ts
@@ -1,26 +1,23 @@
+import type { Data, Page } from "https://deno.land/x/lume@v2.0.3/core/file.ts";
+import type Site from "https://deno.land/x/lume@v2.0.3/core/site.ts";
 import type {
-  Data,
   Engine,
   Helper,
-  Page,
-  Site,
-} from "https://deno.land/x/lume@v1.19.4/core.ts";
-import { Exception } from "https://deno.land/x/lume@v1.19.4/core/errors.ts";
-import { merge } from "https://deno.land/x/lume@v1.19.4/core/utils.ts";
-import loader from "https://deno.land/x/lume@v1.19.4/core/loaders/text.ts";
+} from "https://deno.land/x/lume@v2.0.3/core/renderer.ts";
+import { merge } from "https://deno.land/x/lume@v2.0.3/core/utils/object.ts";
+import loader from "https://deno.land/x/lume@v2.0.3/core/loaders/text.ts";
 import zenn from "npm:zenn-markdown-html@0.1.150";
 import { replaceMath } from "./katex.ts";
+import { Exception } from "./error.ts";
 
 const markdownToHtml = zenn.default;
 
 export interface Options {
   extensions: string[];
-  keepDefaultPlugins: boolean;
 }
 
 export const defaults: Options = {
   extensions: [".md"],
-  keepDefaultPlugins: false,
 };
 
 export class MarkdownEngine implements Engine {
@@ -33,10 +30,10 @@ export class MarkdownEngine implements Engine {
     data?: Data,
     filename?: string,
   ): Promise<string> {
-    return Promise.resolve(this.renderSync(content, data, filename));
+    return Promise.resolve(this.renderComponent(content, data, filename));
   }
 
-  renderSync(
+  renderComponent(
     content: string,
     _data?: Data,
     filename?: string,
@@ -57,10 +54,13 @@ export default function (userOptions?: Partial<Options>) {
   return function (site: Site) {
     const engine = new MarkdownEngine();
 
-    site.loadPages(options.extensions, loader, engine);
+    site.loadPages(options.extensions, {
+      loader,
+      engine,
+    });
 
     function filter(string: string, _inline = false): string {
-      return engine.renderSync(string?.toString() || "").trim();
+      return engine.renderComponent(string?.toString() || "").trim();
     }
 
     site.filter("md", filter as Helper);
@@ -73,4 +73,14 @@ export interface ZennPageData extends Page {
   type: "tech" | "idea";
   topics: string[];
   published: boolean;
+}
+
+/** Extends Helpers interface */
+declare global {
+  namespace Lume {
+    export interface Helpers {
+      /** @see https://lume.land/plugins/markdown/ */
+      md: (string: string, inline?: boolean) => string;
+    }
+  }
 }
